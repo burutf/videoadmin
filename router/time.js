9e6b602881992bc5c1efc48b9ea8736051e795d0
buruTF <104819036+burutf@users.noreply.github.com>
2024-07-07 17:29:02 +0800

    列表的搜索功能和批量删除功能完成

--- router/time.js
+++ router/time.js
@@ -3,12 +3,20 @@
 const express = require('express');
 const router = express.Router();
 //获取服务器时间
-router.get('/gettime',(req,res)=>{
-    const time = new Date().getTime()
-    res.status(200).json({
-        code:200,
-        time
-    })
+router.get('/gettime', (req, res) => {
+    try {
+        const time = new Date().getTime()
+        res.status(200).json({
+            code: 200,
+            time
+        })
+    } catch (error) {
+        res.status(500).json({
+            code: 500,
+            message:'获取时间出错'
+        })
+    }
+
 })
 
 module.exports = router
--- router/videosys.js
+++ router/videosys.js
@@ -10,23 +10,35 @@ router.get('/getvideolist', async (req, res) => {
     //拿到用户信息
     const { isadmin, uuid } = req.userinfo
     //query拿到用户传来的配置信息（pramas是数字已经变成字符串了，需要处理一下）
-    const { page, pagesize,sortobj } = req.query.options
-    try {
-        const arrlist = await findmongo({
-            uuid
-        }, {
-            //排序
-            sort: sortobj,
-            //隐藏字段
-            projection: { _id: 0 },
-            //分页
-            //跳过这些条数
-            skip: (page - 1) * pagesize,
-            //查出这些条数
-            limit: +pagesize
-        })
-        const sumpage = await countdomongo({ uuid })
+    const { page, pagesize,sortobj,datefiltle = [],titlesearch } = req.query.options
 
+    //查找条件项
+    let query = {
+        uuid,
+        //日期范围
+        lastupdate:{$gte:new Date(datefiltle[0]),$lte:new Date(datefiltle[1])},
+        //查找标题
+        title:{$regex:titlesearch}
+    }
+    //如果没有选择日期就删掉查日期范围的属性
+    if (datefiltle.length===0) delete query.lastupdate
+
+    //查找配置项
+    const optobj = {
+        //排序
+        sort: sortobj,
+        //隐藏字段
+        projection: { _id: 0 },
+        //分页
+        //跳过这些条数
+        skip: (page - 1) * pagesize,
+        //查出这些条数
+        limit: +pagesize
+    }
+
+    try {
+        const arrlist = await findmongo(query, optobj)
+        const sumpage = await countdomongo(query)
 
         res.status(200).json({
             code: 200,
@@ -43,7 +55,7 @@ router.get('/getvideolist', async (req, res) => {
     }
 
 })
-
+//删除列表中的一条
 router.delete('/dellist', async (req, res) => {
     const { uuid } = req.userinfo
     const { videoid } = req.query
@@ -59,6 +71,34 @@ router.delete('/dellist', async (req, res) => {
             message: error.message,
         })
     }
+})
+
+//批量删除
+router.delete('/dellistbatch',async (req,res)=>{
+    const { uuid } = req.userinfo
+    const { videoidlist } = req.query
+
+
+    try {
+        //进行并发删除
+        const alllist = videoidlist.map(async e=>{
+            return await delinbatches(uuid,e,true)
+        })
+        await Promise.all(alllist)
+
+        res.status(200).json({
+            code: 200,
+            message: '批量删除成功'
+        })
+    } catch (error) {
+        res.status(error.code).json({
+            code: error.code,
+            message: error.message,
+        })
+    }
+
+
+    
 })
 
 //更改视频列表
