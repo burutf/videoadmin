c43e971f2dace4625b2b37c98151e82c69e1bebe
buruTF <104819036+burutf@users.noreply.github.com>
2024-07-07 23:05:03 +0800

    代码优化

--- router/videosys.js
+++ router/videosys.js
@@ -9,19 +9,23 @@ const { delinbatches } = require('../utils/delinbatches')
 router.get('/getvideolist', async (req, res) => {
     //拿到用户信息
     const { isadmin, uuid } = req.userinfo
-    //query拿到用户传来的配置信息（pramas是数字已经变成字符串了，需要处理一下）
-    const { page, pagesize,sortobj,datefiltle = [],titlesearch } = req.query.options
-
+    //query拿到用户传来的配置信息（pramas是数字已经变成字符串了，做了下处理）
+    const { page, pagesize,sortobj,datefiltle = [],titlesearch,statusff } = req.query.options
     //查找条件项
     let query = {
         uuid,
         //日期范围
         lastupdate:{$gte:new Date(datefiltle[0]),$lte:new Date(datefiltle[1])},
         //查找标题
-        title:{$regex:titlesearch}
+        title:{$regex:titlesearch},
+        //状态筛选
+        status:statusff
     }
     //如果没有选择日期就删掉查日期范围的属性
     if (datefiltle.length===0) delete query.lastupdate
+    //如果statusff没有传值就是不筛选此项
+    if (!statusff) delete query.status
+
 
     //查找配置项
     const optobj = {
