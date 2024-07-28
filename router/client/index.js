05beb5ff727bee40e616a3ebe147f4283fca24c4
buruTF <104819036+burutf@users.noreply.github.com>
2024-07-29 01:46:07 +0800

    获取猜你喜欢列表完善

--- router/client/index.js
+++ router/client/index.js
@@ -38,7 +38,7 @@ router.get("/getbslideshow", async (req, res) => {
 //获取今日更新的视频列表
 router.get("/gettodaylist", async (req, res) => {
   //接收查找条的数量，默认为0，不限制
-  const {pagesize = 0} = req.query
+  const { pagesize = 0 } = req.query;
   // 获取今天的开始和结束时间
   const startOfDay = moment().startOf("day").toDate();
   const endOfDay = moment().endOf("day").toDate();
@@ -57,7 +57,48 @@ router.get("/gettodaylist", async (req, res) => {
     );
     //进行数据处理，根据每条数据添加上代表更新至多少回
     data.forEach((e) => {
-      e.updatenum = e.videolist.length
+      e.updatenum = e.videolist.length;
+    });
+
+    res.status(200).json({
+      code: 200,
+      message: "ok",
+      data,
+    });
+  } catch (error) {
+    res.status(error.code || 500).json({
+      code: error.code || 500,
+      message: error.message,
+    });
+  }
+});
+
+//获取猜你喜欢列表（要获取除去今日更新的列表）
+router.get("/getlikelist", async (req, res) => {
+  //接收查找条的数量和第几次查询，默认为0，不限制
+  const { pagesize = 0, pageindex = 1 } = req.query;
+  // 获取今天的开始和结束时间
+  const startOfDay = moment().startOf("day").toDate();
+  const endOfDay = moment().endOf("day").toDate();
+  try {
+    const data = await findmongo(
+      {
+        lastupdate: { $lt: startOfDay },
+      },
+      {
+        //隐藏字段
+        projection: { _id: 0 },
+        //分页
+        //跳过这些条数
+        skip: (pageindex - 1) * pagesize,
+        //查出这些条数
+        limit: +pagesize,
+      }
+    );
+
+    //进行数据处理，根据每条数据添加上代表更新至多少回
+    data.forEach((e) => {
+      e.updatenum = e.videolist.length;
     });
 
     res.status(200).json({
